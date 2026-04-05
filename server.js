74370ae909abf6496fc8b081d3c91704a02bcd52
RayanEroldMMC <rayan@mentormecollective.org>
2026-04-05 18:34:37 -0400

    🚀 Fix: Update Express 5 wildcard routing to bring site online

--- server.js
+++ server.js
@@ -633,7 +633,7 @@ app.post('/api/create-portal-session', async (req, res) => {
 app.use(express.static(path.join(__dirname, 'dist')));
 
 // Handle SPA routing - deliver index.html for all non-API routes
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
